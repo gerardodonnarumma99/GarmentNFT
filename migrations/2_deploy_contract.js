1b8e23013d19ee0082dac2c77669025f244afe4e
gerardodonnarumma99 <gerardodonnarumma99@gmail.com>
2023-07-03 19:37:00 +0200

    Set Owner Contract in Deploy

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,6 +1,7 @@
 const Garment = artifacts.require("Garment.sol");
 
-module.exports = function(deployer) {
-  // Faccio il deploy del contratto Garment
-  deployer.deploy(Garment, "GarmentContract", "GAR");
+module.exports = function(deployer, network, accounts) {
+  // Faccio il deploy del contratto Garment e imposto l'indirizzo del proprietario
+  console.log('OWNER:', accounts[0])
+  deployer.deploy(Garment, "GarmentContract", "GAR", { from: accounts[0] });
 };
